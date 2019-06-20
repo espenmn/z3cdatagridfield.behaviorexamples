88c0148c85a1a0074d296ad165052bfd0d244ee3
Espen Moe-Nilssen <espen@medialog.no>
2019-06-20 15:17:18 +0200

    fix typos

--- src/z3cdatagridfield/behaviorexamples/views/data_grid_demo_view.py
+++ src/z3cdatagridfield/behaviorexamples/views/data_grid_demo_view.py
@@ -2,7 +2,7 @@
 
 from z3cdatagridfield.behaviorexamples import _
 from Products.Five.browser import BrowserView
-
+from plone import api
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -16,11 +16,21 @@ class DataGridDemoView(BrowserView):
         return self.index()
 
     def get_table(self):
-        return self.context.table
+        #return self.context.table
         # I think we should get the relations here
         # by looking at the uid of the family relatioon
+        # we should walk through all
+        # and look up item object
+        table_list = []
+        for row in self.context.table:
+            rel = row['family_relation'].UID()
+            item = api.content.get(UID=rel)
+            row.update({'uid': rel, 'item': item})
+            table_list.append(row)
+        return table_list
 
 
     def get_sorted_table(self):
-        my_table = self.context.table
+        #my_table = self.context.table
+        my_table = self.get_table()
         return sorted(my_table, key=lambda k: k['last_name'])
